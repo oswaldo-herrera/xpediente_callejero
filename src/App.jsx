c99ebb068f0f8419bd29799eb0d65c9b5b8efcdc
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2026-06-28 23:02:13 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="coming-soon">
+      <h1>Próximamente</h1>
+      <p>Xpediente Callejero</p>
+    </div>
+  )
+}
+
+export default App
